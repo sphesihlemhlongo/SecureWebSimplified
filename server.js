21d5ab3c68340548e945cceb9349b1738a016dff
Sphesihle Mhlongo <mhlongosihle49@gmail.com>
2024-12-11 13:41:58 +0200

    Logging

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 const express = require('express');
-const path = require('path');
+const morgan = require('morgan');
 const httpVsHttpsRoutes = require('./routes/httpVsHttps'); // Import the HTTP/HTTPS route handler
 
 const app = express();
 const PORT = 3000;
 
 
-
+// Middleware: Use Morgan to log HTTP requests
+app.use(morgan('combined'));
 
 // Serve static files from the public folder
 app.use(express.static('public'));
@@ -17,4 +18,10 @@ app.use(httpVsHttpsRoutes);
 // Start the server
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
+});
+
+// Error-handling middleware
+app.use((err, req, res, next) => {
+    console.error(`Error occurred: ${err.message}`);
+    res.status(500).send('Something went wrong!');
 });
